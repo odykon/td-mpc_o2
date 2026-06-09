6de2d54ead50054fa53053d3125cbf1b2a797735
Odysseas Konias <odysseaskonias@Odysseass-MacBook-Air.local>
2026-06-09 12:53:43 +0300

    experiment setup

--- scripts/run_cartpole_acrobot.sh
+++ scripts/run_cartpole_acrobot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=cheetah-run seed=1 use_wandb=True
-python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=cheetah-run seed=2 use_wandb=True
+python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=cheetah-run seed=5 use_wandb=True
+python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=cheetah-run seed=6 use_wandb=True
 
-python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=walker-walk seed=1 use_wandb=True
-python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=walker-walk seed=2 use_wandb=True
+python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=walker-walk seed=5 use_wandb=True
+python3 scripts/train_o2_phased.py cfg=cfgs/exp_phased.yaml task=walker-walk seed=6 use_wandb=True
